e6ae14f53f3a5ddee07fc03fab4f849b6b2c5411
JeremyWeed <jweed262@gmail.com>
2015-05-03 11:41:34 -0400

    more setup

--- desktop/src/com/spacegdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/spacegdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.spacegdx.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Space Game";
+		config.width =  800;
+		config.height = 450;
 		new LwjglApplication(new Game(), config);
 	}
 }
