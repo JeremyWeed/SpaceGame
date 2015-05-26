0ca033abd131399881148e4c1c494c0d9e1055cd
JeremyWeed <jweed262@gmail.com>
2015-05-25 22:02:41 -0400

    asteroids rotate

--- core/src/com/spacegdx/game/Enemies/Asteroid.java
+++ core/src/com/spacegdx/game/Enemies/Asteroid.java
@@ -1,18 +1,19 @@
 package com.spacegdx.game.Enemies;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.spacegdx.game.Enemy;
-import com.spacegdx.game.EnemyLaser;
+        import com.badlogic.gdx.Gdx;
+        import com.badlogic.gdx.graphics.Texture;
+        import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+        import com.badlogic.gdx.math.MathUtils;
+        import com.badlogic.gdx.math.Rectangle;
+        import com.spacegdx.game.Enemy;
+        import com.spacegdx.game.EnemyLaser;
 
 /**
  * Created by jeremy on 5/23/15.
  */
 public class Asteroid extends Enemy{
     double xSpeed, ySpeed;
+    float rSpeed, rAngle;
     enum Side{
         left,
         right
@@ -28,6 +29,7 @@ public class Asteroid extends Enemy{
 
         xSpeed = speed * Math.cos(angle);
         ySpeed = speed * Math.sin(angle);
+        rSpeed = MathUtils.random(-90, 90);
 
         if(side == Side.right){
             hitbox = new Rectangle(480,MathUtils.random(200, 800), width, height);
@@ -48,11 +50,14 @@ public class Asteroid extends Enemy{
 
     @Override
     public void draw(SpriteBatch sb) {
-        sb.draw(t,hitbox.x, hitbox.y);
+        sb.draw(t, hitbox.x, hitbox.y, width / 2, height / 2, width, height, 1, 1,
+                rAngle, 0, 0, (int)width, (int)height, false, false);
     }
 
     @Override
     public void move() {
+        rAngle += rSpeed * Gdx.graphics.getDeltaTime();
+        rAngle = wrap(rAngle);
         hitbox.x -= xSpeed * Gdx.graphics.getDeltaTime();
         hitbox.y -= ySpeed * Gdx.graphics.getDeltaTime();
     }
@@ -65,5 +70,19 @@ public class Asteroid extends Enemy{
     @Override
     public EnemyLaser spawnLaser() {
         return null;
+    }
+
+    // lol stealing from robotics code
+    public float wrap(float r){
+        //return (r > desiredAngle) ? 360 - (r - desiredAngle) : desiredAngle - r;
+        //return (r > 180) ? wrap(r - 360) : (r <= -180) ? wrap(r + 360) : r;
+        if (Math.abs(r) >= 180) {
+            r = r % 360;
+            return (r > 180) ? r - 360 : (r <= -180) ? r + 360 : r;
+        } else {
+            return r;
+        }
+
+        //looks like the gyro is +-180, so I copied the code from last year, and modified it for angles instead of radians
     }
 }
